b16518746c6ba1bd9de63faf753e42bf9b383f62
Gonzalo <gompatri@gmail.com>
2023-11-12 23:46:32 -0300

    mongo db

---
+++ src/database.py
@@ -0,0 +1,25 @@
+import pymongo, os
+from dotenv import load_dotenv
+
+
+load_dotoenv()
+db_uri = os.getenv("DB_URI")
+
+def db_connect():
+    connection = pymongo.MongoClient(db_uri)
+    return connection
+
+def register(conn, ctx):
+    database = conn["discord_server"]
+    collection = database["user"]
+    doc= {"discordID" : str(ctx.author.id), "userName": str(ctx.author)}
+    collection.insert_one(doc)
+
+def verify_id(conn, discord_ID):
+    database = conn["discord_server"]
+    collection = database["users"]
+    doc = collection.find_one({"discordID": discord_ID})
+    if doc:
+        return True
+    else:
+        return False
